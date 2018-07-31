607bd0dbe36dd2f8aadc83c746065b8b25dc9564
Emir Mandal <emir.enetel@gmail.com>
2018-07-31 21:01:06 +0200

    tuples

--- Chapter05/PacktLibrary/Person.cs
+++ Chapter05/PacktLibrary/Person.cs
@@ -45,5 +45,22 @@ namespace Packt.CS7
             return $"{Name} was born on {HomePlanet}";
         }
 
+        // the old C# 4 and .NET 4.0 System.Tuple type
+        public Tuple<string, int> GetFruitCS4()
+        {
+            return Tuple.Create("Apples", 5);
+        }
+
+        // the new C# 7 syntax and new System.ValueTuple
+        public (string, int) GetFruitCS7()
+        {
+            return ("Apples", 5);
+        }
+
+        public (string Name, int Number) GetNamedFruit()
+        {
+            return (Name: "Apples", Number: 5);
+        }
+
     }
 }
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -59,6 +59,24 @@ namespace PeopleApp
 
             p1.WriteToConsole();
             WriteLine(p1.GetOrigin());
+
+            Tuple<string, int> fruit4 = p1.GetFruitCS4();
+            WriteLine($"There are {fruit4.Item2} {fruit4.Item1}.");
+
+            (string, int) fruit7 = p1.GetFruitCS7();
+            WriteLine($"{fruit7.Item1}, {fruit7.Item2} there are.");
+
+            var fruitNamed = p1.GetNamedFruit();
+            WriteLine($"Are there {fruitNamed.Number} {fruitNamed.Name}?");
+
+            var thing1 = ("Neville", 4);
+            WriteLine($"{thing1.Item1} has {thing1.Item2} children");
+            var thing2 = (p1.Name, p1.Children.Count);
+            WriteLine($"{thing2.Name} has {thing2.Count} children.");
+
+            (string fruitName, int fruitNumber) = p1.GetFruitCS7();
+            WriteLine($"Deconstructed: {fruitName}, {fruitNumber}");
+
         }
     }
 }
