9bd7798621e48ca835a32b0f14e1d9e120ccfe02
Emir Mandal <emir.enetel@gmail.com>
2018-07-31 19:17:55 +0200

    default literals

---
+++ Chapter05/PacktLibrary/ThingOfDefaults.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Packt.CS7;
+
+namespace PacktLibrary
+{
+    class ThingOfDefaults
+    {
+        public int Population;
+        public DateTime When;
+        public string Name;
+        public List<Person> People;
+
+        public ThingOfDefaults()
+        {
+            //Population = default(int); // C# 2.0 and later
+            //When = default(DateTime);
+            //Name = default(string);
+            //People = default(List<Person>);
+
+            Population = default;
+            When = default;
+            Name = default;
+            People = default;
+        }
+    }
+}
