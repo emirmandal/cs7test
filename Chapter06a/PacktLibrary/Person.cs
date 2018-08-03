76c1bc02653718146c8ff754b124d912e95e5544
Emir Mandal <emir.enetel@gmail.com>
2018-08-04 01:45:52 +0200

    added Chapter06a

---
+++ Chapter06a/PacktLibrary/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace Packt.CS7
+{
+    public partial class Person
+    {
+        // fields
+        public string Name;
+        public DateTime DateOfBirth;
+        public List<Person> Children = new List<Person>();
+
+        // methods.
+        public void WriteToConsole()
+        {
+            WriteLine($"{Name} was born on {DateOfBirth:dddd, d MMMM yyyy}");
+        }
+
+        // methods to "multiply"
+        public static Person Procreate(Person p1, Person p2)
+        {
+            var baby = new Person
+            {
+                Name = $"Baby of {p1.Name} and {p2.Name}"
+            };
+            p1.Children.Add(baby);
+            p2.Children.Add(baby);
+            return baby;
+        }
+
+        public Person ProcreateWith(Person partner)
+        {
+            return Procreate(this, partner);
+        }
+
+        // operator to "multiply"
+        public static Person operator *(Person p1, Person p2)
+        {
+            return Person.Procreate(p1, p2);
+        }
+
+        // method with a local function
+        public static int Factorial(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException($"{nameof(number)} cannot be less that zero.");
+            }
+            return localFactorial(number);
+
+            int localFactorial(int localNumber)
+            {
+                if (localNumber < 1) return 1;
+                return localNumber * localFactorial(localNumber - 1);
+            }
+        }
+    }
+}
